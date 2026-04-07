851e5a8829a9e3068ccdd8fc1a7b373b8b855c35
yL-213 <63343227+yL-213@users.noreply.github.com>
2026-04-07 11:53:58 +0800

    home

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,9 +1,14 @@
 function Home() {
-  return (
+    return (
     <div className="mx-2">
       <h1 className="text-xl font-medium px-2">Home Page</h1>
       <div className="m-4 p-4 border-2 border-indigo-400 rounded">
-        You may put your personal information here.
+        <p className="text-2xl font-bold mb-4">Welcome!</p>
+        <img
+          src="https://picsum.photos/400/200"
+          alt="welcome"
+          className="rounded"
+        />
       </div>
     </div>
   );
